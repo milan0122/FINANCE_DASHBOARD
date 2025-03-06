9029a26f6fae449130aa761cf9d5a29a4c309986
Milan <dangi.milan46@gmail.com>
2025-03-07 00:43:50 +0545

    defining layout

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,14 +1,89 @@
-import React from 'react';
-import { Box,useTheme } from '@mui/material';
-
+import { Box, useMediaQuery, useTheme } from '@mui/material';
 
 type Props = {};
 
-const Dashboard = (props: Props) => {
-    const {palette} = useTheme();
+const gridTemplateLS = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f" 
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+
+const gridTemplateSS = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
+
+const Dashboard = (_props: Props) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1200px)");
+  const { palette } = useTheme();
+
   return (
-    <Box color={palette.grey[300]}>Dashboard</Box>
-  )
-}
+    <Box
+      width="100%"
+      height="100%"
+      display="grid"
+      gap="1.5rem"
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3,minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10,minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLS,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSS,
+            }
+      }
+    >
+      <Box bgcolor="#fff" gridArea="a"></Box>
+      <Box bgcolor="#fff" gridArea="b"></Box>
+      <Box bgcolor="#fff" gridArea="c"></Box>
+      <Box bgcolor="#fff" gridArea="d"></Box>
+      <Box bgcolor="#fff" gridArea="e"></Box>
+      <Box bgcolor="#fff" gridArea="f"></Box>
+      <Box bgcolor="#fff" gridArea="g"></Box>
+      <Box bgcolor="#fff" gridArea="h"></Box>
+      <Box bgcolor="#fff" gridArea="i"></Box>
+      <Box bgcolor="#fff" gridArea="j"></Box>
+    </Box>
+  );
+};
 
 export default Dashboard;
