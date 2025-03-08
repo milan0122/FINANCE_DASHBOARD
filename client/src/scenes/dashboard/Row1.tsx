2e3ab5fe350777d758d35cd60b809e68608a7c6c
Milan <dangi.milan46@gmail.com>
2025-03-08 12:59:13 +0545

    row1 completed

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -5,12 +5,24 @@ import { useTheme } from "@mui/material";
 import Header from "@/components/Header";
 import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Rectangle, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
-type Props = {};
 
-const Row1 = (props: Props) => {
+
+const Row1 = () => {
   const { data } = useGetKpisQuery();
   const { palette } = useTheme();
   console.log('data:', data);
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+        }
+      })
+    );
+  }
+    , [data]);
   const revenueExpenses = useMemo(() => {
     return (
       data &&
@@ -119,14 +131,17 @@ const Row1 = (props: Props) => {
             margin={{
               top: 20,
               right: 0,
-              left:5 ,
+              left:- 5,
               bottom: 55,
             }}
           >
             <CartesianGrid vertical={false} stroke={palette.grey[800]} />
 
-            <XAxis dataKey= "name" tickLine={false} 
-            syle={{fontSize:"10px"}}/>
+            <XAxis 
+            dataKey="name" 
+            tickLine={false} 
+            syle= {{ fontSize:"10px"}}
+            />
 
             <YAxis yAxisId="left" orientation="left"
             tickLine={false} 
@@ -168,24 +183,40 @@ const Row1 = (props: Props) => {
         subtitle="bar chart represent revenue month by month"
         sideText="+4%"
         />
+
         <BarChart
           width={500}
           height={300}
-          data={data}
+          data={revenue}
           margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
+            top: 15,
+            right: 15,
+            left: -5,
+            bottom: 60,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <defs>
+          <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop 
+                offset="5%" 
+                stopColor={palette.primary[300]}
+                 stopOpacity={0.8}/>
+                 <stop 
+                offset="95%" 
+                stopColor={palette.primary[300]}
+                 stopOpacity={0}/>
+                 </linearGradient>
+              </defs>
+          <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
+          <XAxis dataKey="name" 
+          axisLine={false} tickLine={false} style={{fontSize:"10px"}} 
+          />
+          <YAxis axisLine={false} tickLine={false} style={{fontSize:"10px"}} />
           <Tooltip />
           <Legend />
-          <Bar dataKey="month" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-          <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+          <Bar dataKey="revenue" 
+            fill="url(#colorRevenue)"/>
+          {/* <Bar dataKey="uv" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} /> */}
         </BarChart>
       </ResponsiveContainer>
       </DashboardBox>
