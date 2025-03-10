ba624dc6df8a65433f6276bba54f0935c6b7fe5d
Milan <dangi.milan46@gmail.com>
2025-03-11 00:09:08 +0545

    dashboard completed

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,17 +1,36 @@
 import DashboardBox from '@/components/DashboardBox';
+import FlexBetween from '@/components/FlexBetween';
 import Header from '@/components/Header';
 import { useGetKpisQuery, useGetProductsQuery, useGetTransactionsQuery } from '@/state/api';
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { DataGrid, GridCellParams } from '@mui/x-data-grid';
 import React, { useMemo } from "react";
-type Props = {};
+import { Cell, Pie, PieChart } from 'recharts';
 
-const Row3 = (props: Props) => {
-  const { data: KPIData } = useGetKpisQuery();
+const Row3 = () => {
+  const { data: KpiData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   const { data: transactionData } = useGetTransactionsQuery();
   const { palette } = useTheme();
+  const pieColor = [palette.primary[800],palette.primary[500]]
+  const pieChartData= useMemo(()=>{
+    if(KpiData){
+    const totalExpenses= KpiData[0].totalExpenses;
+    return Object.entries(KpiData[0].expensesByCategory).map(
+      ([key,value])=>{
+        return [
+          {
+            name:key,
+            value:value,
+          },{
+            name:`${key} of Total`,
+            value:totalExpenses-value,
+          },
+        ];
+      }
+    )
 
+  }},[KpiData]);
   const productColumns = [
     {
       field: '_id',
@@ -72,7 +91,7 @@ const Row3 = (props: Props) => {
               border: "none"
             },
             "& .MuiDataGrid-cell": {
-              borderBottom: `1px solid ${palette.grey[800]}`  
+              borderBottom: `1px solid ${palette.grey[800]}`
             },
             "& .MuiDataGrid-columnHeaders": {
               borderBottom: `1px solid ${palette.grey[800]}`
@@ -108,7 +127,7 @@ const Row3 = (props: Props) => {
               border: "none"
             },
             "& .MuiDataGrid-cell": {
-              borderBottom: `1px solid ${palette.grey[800]}`  
+              borderBottom: `1px solid ${palette.grey[800]}`
             },
             "& .MuiDataGrid-columnHeaders": {
               borderBottom: `1px solid ${palette.grey[800]}`
@@ -129,8 +148,67 @@ const Row3 = (props: Props) => {
         </Box>
 
       </DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
-      <DashboardBox gridArea="j"></DashboardBox>
+
+
+      <DashboardBox gridArea="i">
+        <Header
+          title="Expenses Breakdown By Category"
+          sideText='+4%' />
+        <FlexBetween mt="0.5rem" gap="0.5rem" textAlign="center">
+          {pieChartData?.map((data,i)=>(
+            <Box key={`$data[0].name-${i}`}>
+            <PieChart
+              width={100}
+              height={75}
+            >
+              <Pie
+                stroke='none'
+                data={data}
+                innerRadius={18}
+                outerRadius={35}
+                paddingAngle={2}
+                dataKey="value"
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={pieColor[index]} />
+                ))}
+              </Pie>
+            </PieChart>
+     
+              <Typography mb="0.5rem" variant="h5"> {data[0].name} </Typography>
+           
+            </Box>
+
+          ))}
+        </FlexBetween>
+
+      </DashboardBox>
+      <DashboardBox gridArea="j">
+        <Header
+        title='Overall Sumamry and Explaination Data'
+        sideText='+15%'
+        />
+        <Box
+        height="15px"
+        margin="1.25rem 1rem 0.4rem 1rem"
+        borderRadius="1rem"
+        bgcolor={palette.primary[400]}
+        >
+          <Box
+          height="15px"
+          bgcolor={palette.primary[600]}
+          borderRadius="1rem"
+          width="40%"
+          >
+          </Box>
+          <Typography margin="0.5rem 1rem" variant='h6'>
+            Odit, ab ipsa repellendus nulla illo accusamus nesciunt natus maiores animi a fugit iusto incidunt sint, ex aperiam numquam error ad aliquam quod deserunt neque tenetur? Similique, aspernatur! Quod, reiciendis?
+          </Typography>
+
+        </Box>
+      </DashboardBox>
     </>
   )
 }
